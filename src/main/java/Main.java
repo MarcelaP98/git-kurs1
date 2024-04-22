f7e26c99039f30c3bacf03e19fe0f7165be0c669
MarcelaP98 <wrx91803@student.wroclaw.merito.pl>
2024-04-22 16:22:11 +0200

    Zmiana druga w repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Mateusz");
     }
+    private void displayText() {
+        System.out.println("Zmiana druga w repo");
+    }
 }
